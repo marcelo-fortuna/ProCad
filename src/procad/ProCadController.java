c6260b434f712739e17b6a3359b0dd87b36c3c8e
Marcelo Fortuna <celofortuna@gmail.com>
2023-11-09 22:38:11 -0300

    Creating Model and Controller classes
    + Adding attributes;
    + Adding getters.

---
+++ src/procad/ProCadController.java
@@ -0,0 +1,27 @@
+package procad;
+
+/**
+ *
+ * @author Marcelo
+ * @since 09/11/2023
+ */
+public class ProCadController {
+    
+    public void clear() {
+//        txtCod.setText("");
+//        txtProductName.setText("");;
+//        txtStockQuantity.setText("");
+//        txtProfitFactor.setText("");
+//        
+//        fmtRegisterDate.setText("");
+//        fmtBuyPrice.setText("");
+//        fmtSellPrice.setText("");
+//        fmtNCM.setText("");
+//        fmtBarCode.setText("");
+//        
+//        txaDescription.setText("");
+//        
+//        spiMinStock.setValue(0);
+//        spiMaxStock.setValue(0);
+    }
+}
