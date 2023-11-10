815d836c3ea294029704f036b7f4ead537afb239
Marcelo Fortuna <celofortuna@gmail.com>
2023-11-10 16:09:15 -0300

    Updating getter setters and logic of the database string connection

--- src/procad/Data/DataModel.java
+++ src/procad/Data/DataModel.java
@@ -3,6 +3,7 @@ package procad.Data;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -13,51 +14,61 @@ public class DataModel {
     
     private Connection cn;
     
-    private String dbconnection;
+    private String database = "ProCad";
     private String username;
     private String password;
     
+    /**
+     * Method to set username of the connection with the database
+     * @param username
+     * @return 
+     */
     public String setUsername(String username) {
         return this.username = username;
     }
-    
-    public String getUsername() {
-        return this.username;
-    }
  
+    /**
+     * Method to set password of the connection with the database
+     * @param password
+     * @return 
+     */
     public String setPassword(String password) {
         return this.password = password;
     }
     
-    public String getPassword() {
-        return this.password;
+    /**
+     * Method to set the database in connection string.
+     * @param database
+     * @return 
+     */
+    public String setDatabaseInConnectionString(String database) {
+        return this.database = database;
     }
     
-    public String setDatabaseConnectionString(String prefix, String url, int port, String database) {
-        return this.dbconnection = prefix + "://" + url + ":" + String.valueOf(port) + "/" + database;
-    }
-    
-    public String getDatabaseConnectionString() {
-        return this.dbconnection;
+    public String getDatabaseInConnectionString() {
+        return this.database;
     }
 
+    /**
+     * Method to stabilish the connection with the database.
+     */
     public void connect() {
         try {
-            cn = DriverManager.getConnection(getDatabaseConnectionString(), getUsername(), getPassword());
+            cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + this.database, this.username, this.password);
         } catch (SQLException e) {
-            System.out.println("""
-                               Erro ao tentar se conectar com o banco de dados.
-                               Mensagem: """ + e.getMessage());
+            JOptionPane.showMessageDialog(null,"Erro ao tentar se conectar com o banco de dados. Não é possível iniciar o programa.\nContate o desenvolvedor...\n" + "Mensagem: " + e.getMessage());
+            System.exit(-1);
         }
     }
     
+    /**
+     * Method to close the connection with the database.
+     */
     public void disconnect() {
         try {
             cn.close();
         } catch (SQLException e) {
-            System.out.println("""
-                               Erro ao tentar se desconectar do banco de dados.
-                               Mensagem: """ + e.getMessage());
+            JOptionPane.showMessageDialog(null,"Erro ao tentar se desconectar do banco de dados.\nContate o desenvolvedor...\n" + "Mensagem: " + e.getMessage());
         }
     }
 }
