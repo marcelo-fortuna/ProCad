d82b4a0b3afb0cdee6a364c37d79d1931030337a
Marcelo Fortuna <celofortuna@gmail.com>
2023-11-14 17:46:51 -0300

    Implementing ProCadController
    + Add data formatter to fmtRegisterDate
    + Add clear of all fields.

--- src/procad/ProCadController.java
+++ src/procad/ProCadController.java
@@ -1,5 +1,9 @@
 package procad;
 
+import java.text.ParseException;
+import javax.swing.JOptionPane;
+import javax.swing.text.MaskFormatter;
+
 /**
  *
  * @author Marcelo
@@ -7,21 +11,33 @@ package procad;
  */
 public class ProCadController {
     
+    ProCad procad = new ProCad();
+    
+    public void dateFormatter() {
+
+        try {
+            MaskFormatter mask = new MaskFormatter("##/##/####");
+            mask.install(procad.fmtRegisterDate);
+        } catch (ParseException ex) {
+            JOptionPane.showConfirmDialog(null, "Erro ao formatar campo de texto.", "Mensagem de erro" ,JOptionPane.ERROR);
+        }
+    }
+    
     public void clear() {
-//        txtCod.setText("");
-//        txtProductName.setText("");;
-//        txtStockQuantity.setText("");
-//        txtProfitFactor.setText("");
-//        
-//        fmtRegisterDate.setText("");
-//        fmtBuyPrice.setText("");
-//        fmtSellPrice.setText("");
-//        fmtNCM.setText("");
-//        fmtBarCode.setText("");
-//        
-//        txaDescription.setText("");
-//        
-//        spiMinStock.setValue(0);
-//        spiMaxStock.setValue(0);
+        procad.txtCod.setText("");
+        procad.txtProductName.setText("");;
+        procad.txtStockQuantity.setText("");
+        procad.txtProfitFactor.setText("");
+        
+        procad.fmtRegisterDate.setText("");
+        procad.fmtBuyPrice.setText("");
+        procad.fmtSellPrice.setText("");
+        procad.fmtNCM.setText("");
+        procad.fmtBarCode.setText("");
+        
+        procad.txaDescription.setText("");
+        
+        procad.spiMinStock.setValue(0);
+        procad.spiMaxStock.setValue(0);
     }
 }
